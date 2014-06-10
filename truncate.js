a8187ae401da0bac85626a0e2c49c0a9dd92e445
James Hrisho <james.hrisho@gmail.com>
2014-06-10 09:10:26 -0400

    redone truncate

--- truncate.js
+++ truncate.js
@@ -4,64 +4,64 @@
  * @module Utility
  */
 (function (context, undefined) {
-    'use strict';
+  'use strict';
 
-    var DEFAULT_TRUNCATE_SYMBOL = '...',
-        URL_REGEX               = /(((ftp|https?):\/\/)[\-\w@:%_\+.~#?,&\/\/=]+)|((mailto:)?[_.\w-]+@([\w][\w\-]+\.)+[a-zA-Z]{2,3})/g; // Simple regexp
+  var DEFAULT_TRUNCATE_SYMBOL = '...',
+      URL_REGEX               = /(((ftp|https?):\/\/)[\-\w@:%_\+.~#?,&\/\/=]+)|((mailto:)?[_.\w-]+@([\w][\w\-]+\.)+[a-zA-Z]{2,3})/g; // Simple regexp
 
-    function __appendEllipsis(string, options, content){
-        if(content.length === string.length || !options.ellipsis){return content;}
-        content += options.ellipsis;
-        return content;
-    }
-    /**
-     * Truncate HTML string and keep tag safe.
-     *
-     * @method truncate
-     * @param {String} string string needs to be truncated
-     * @param {Number} maxLength length of truncated string
-     * @param {Object} options (optional)
-     * @param {Boolean} [options.keepImageTag] flag to specify if keep image tag, false by default
-     * @param {Boolean|String} [options.ellipsis] omission symbol for truncated string, '...' by default
-     * @return {String} truncated string
-     */
-    function truncate(str, maxLength, options) {
-        var content = '',         // truncated text storage
-            matches = true,
-            remainingLength = maxLength,
-            result,
-            index;
+  function __appendEllipsis(string, options, content){
+    if(content.length === string.length || !options.ellipsis){return content;}
+    content += options.ellipsis;
+    return content;
+  }
+  /**
+   * Truncate HTML string and keep tag safe.
+   *
+   * @method truncate
+   * @param {String} string string needs to be truncated
+   * @param {Number} maxLength length of truncated string
+   * @param {Object} options (optional)
+   * @param {Boolean} [options.keepImageTag] flag to specify if keep image tag, false by default
+   * @param {Boolean|String} [options.ellipsis] omission symbol for truncated string, '...' by default
+   * @return {String} truncated string
+   */
+  function truncate(str, maxLength, options) {
+      var content = '',         // truncated text storage
+          matches = true,
+          remainingLength = maxLength,
+          result,
+          index;
 
-        options          = options || {};
-        options.ellipsis = (typeof options.ellipsis === "undefined") ? DEFAULT_TRUNCATE_SYMBOL : options.ellipsis;
+      options          = options || {};
+      options.ellipsis = (typeof options.ellipsis === "undefined") ? DEFAULT_TRUNCATE_SYMBOL : options.ellipsis;
 
-        if(!str || str.length === 0){
-            return '';
+      if(!str || str.length === 0){
+        return '';
+      }
+
+      var bits, i;
+      if (typeof str !== "string") {
+        return '';
+      }
+      bits = str.split('');
+      if (bits.length > maxLength) {
+        for (i = bits.length - 1; i > -1; --i) {
+          if (i > maxLength) {
+            bits.length = i;
+          }
+          else if (' ' === bits[i]) {
+            bits.length = i;
+            break;
+          }
         }
-
-        var bits, i;
-        if (string !== typeof str) {
-        	return '';
-        }
-      	bits = str.split('');
-      	if (bits.length > limit) {
-      		for (i = bits.length - 1; i > -1; --i) {
-      			if (i > limit) {
-      				bits.length = i;
-      			}
-      			else if (' ' === bits[i]) {
-      				bits.length = i;
-      				break;
-      			}
-      		}
-      		bits.push('...');
-      	}
-      	return bits.join('');
+        bits.push('...');
+      }
+      return bits.join('');
     }
 
-    if ('undefined' !== typeof module && module.exports) {
-        module.exports = truncate;
-    } else {
-        context.truncate = truncate;
-    }
+  if ('undefined' !== typeof module && module.exports) {
+    module.exports = truncate;
+  } else {
+    context.truncate = truncate;
+  }
 }(String));
